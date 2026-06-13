5445454752283b81dcfa7b3438e9e6489b5baecc
Leonardo <leonardopinheirosilva16@gmail.com>
2026-06-13 16:36:43 -0300

    chore: add Firebase config

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+// @ts-ignore: suppress module not found error if types are missing in the environment
+import { initializeApp } from 'firebase/app'
+// @ts-ignore: suppress module not found error if types are missing in the environment
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfWyqpT15ty1YxE_HLH9_FEzzMcrcjUPs',
+  authDomain: 'club-ecommerce-cffb1.firebaseapp.com',
+  projectId: 'club-ecommerce-cffb1',
+  storageBucket: 'club-ecommerce-cffb1.firebasestorage.app',
+  messagingSenderId: '715341504321',
+  appId: '1:715341504321:web:f2ccb21616e0282b250bc0',
+  measurementId: 'G-CKP72Y368H'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
